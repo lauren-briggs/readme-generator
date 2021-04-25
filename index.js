1442c81850bf8150e76b67db32fe1c4e770b39e2
lauren-briggs <laurenbriggs41@gmail.com>
2021-04-25 11:58:30 +1000

    Updated readme and added questions to array

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe your project",
+    }
+    {
+        type: "input",
+        name: "installation instructions",
+        message: "What are the steps required to install your project?",
+    }
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide instructions and examples for use."
+    }
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please describe how other developers can contribute."
+    }
+    {
+        type: "input",
+        name: "testing",
+        message: "What are the testing instructions?"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
