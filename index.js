b0c75a83803b3166fee1369a9b07931a4e34482b
lauren-briggs <laurenbriggs41@gmail.com>
2021-04-25 12:17:17 +1000

    initialised inquirer npm

--- index.js
+++ index.js
@@ -16,47 +16,47 @@ const questions = [
         name: "description",
         message: "Please describe your project",
         default: "This is my new project.",
-    }
+    },
     {
         type: "input",
         name: "installation",
         message: "What are the steps required to install your project?",
         default: "No installation instructions required."
-    }
+    },
     {
         type: "input",
         name: "usage",
         message: "Please provide instructions and examples for use.",
         default: "No usage instructions required.",
-    }
+    },
     {
         type: "input",
         name: "contribution",
         message: "Please describe how other developers can contribute.",
         default: "Contributors welcome",
-    }
+    },
     {
         type: "input",
         name: "testing",
         message: "What are the testing instructions?",
         default: "No testing instructions required."
-    }
+    },
     {
         type: "checkbox",
         name: "license",
         message: "Choose a license for your project",
         choices: ["Apache license 2.0", "GNU GPLv3", "MIT", "Mozilla Public License 2.0"],
-    }
+    },
     {
         type: "input",
         name: "username",
         message: "Please enter your github username.",
-    }
+    },
     {
         type: "input",
         name: "email",
         message: "Please enter your email address.",
-    }
+    },
 ];
 
 // TODO: Create a function to write README file
@@ -67,3 +67,5 @@ function init() { }
 
 // Function call to initialize app
 init();
+
+inquirer.prompt(questions);
